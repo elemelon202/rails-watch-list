d3bfe2a8d100f69cf52c7fd129c5e79c834537f7
Sam Bulman <161944461+elemelon202@users.noreply.github.com>
2025-11-13 15:34:55 +0900

    Basic app functioning

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,22 @@
 class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = @list.bookmarks.new
+  end
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = @list.bookmarks.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_content
+    end
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
 end
