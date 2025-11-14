d26341759bbd39de863ef2cad9d289784f719f70
Sam Bulman <161944461+elemelon202@users.noreply.github.com>
2025-11-14 13:51:31 +0900

    changed

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,5 +2,8 @@ class List < ApplicationRecord
     has_many :bookmarks, dependent: :destroy
     has_many :movies, through: :bookmarks
     has_one_attached :photo
-    validates :name, uniqueness: true, presence: true
+    validates :name, uniqueness: true, presence: true, length: {
+    maximum: 10,
+    message: "cannot be longer than 10 characters"
+  }
 end
